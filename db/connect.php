c8ce041404a49e892e527cc2f38fe52107ab26d4
mic050r <103114387+mic050r@users.noreply.github.com>
2023-09-28 19:48:55 +0900

    fix : 장바구니 userid에 root값이 들어가는 오류 해결

--- db/connect.php
+++ db/connect.php
@@ -1,11 +1,11 @@
 <?php
 $servername = "localhost";
-$username = "root";
+$name = "root";
 $password = "1234";
 $dbname = "starbucks";
 $port = "3308";
 
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
+$conn = new mysqli($servername, $name, $password, $dbname, $port);
 
 if ($conn->connect_error) {
     die("데이터베이스 연결 실패: " . $conn->connect_error);
